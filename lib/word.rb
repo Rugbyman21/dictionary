f191a42f60c19d74956138762dd39f4ce6feadd3
Rugbyman21 <trevorscherling@Trevors-MacBook-Air.local>
2015-09-06 12:51:24 -0700

    second spec pass

--- lib/word.rb
+++ lib/word.rb
@@ -6,7 +6,7 @@ class Word
   define_method(:initialize) do |attributes|
       @name = attributes.fetch(:name)
       # @vocab = []
-      # @id = @@word.length().+(1)
+      @id = @@word.length().+(1)
 
   end
 
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -13,6 +13,12 @@ require('word')
     end
   end
 
+  describe('#id') do
+    it("returns the id of the word") do
+      test_word = Word.new({:name => "car", :id => "1"})
+      expect(test_word.id()).to(eq(1))
+    end
+  end
 
 
 
