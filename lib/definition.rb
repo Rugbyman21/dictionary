196b13af64b7e16280df90150260dc6d3118c453
Rugbyman21 <trevorscherling@Trevors-MacBook-Air.local>
2015-09-06 14:38:25 -0700

    passing spec

--- lib/definition.rb
+++ lib/definition.rb
@@ -1,12 +1,19 @@
 class Definition
-  attr_reader(:description)
+  attr_reader(:description, :id)
 
   @@definitions = []
 
   define_method(:initialize) do |attribute|
     @description = attribute.fetch(:description)
-    # @type = attribute.fetch(:type)
-    # @id = @@definitions.length().+(1)
+    @id = @@definitions.length().+(1)
   end
+
+  define_singleton_method(:all) do
+   @@definitions
+ end
+
+  # define_method(:save) do
+  #   @@definitions.push(self)
+  # end
 
 end
--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -8,10 +8,25 @@ describe(Definition) do
 
   describe('description') do
     it("returns definitions of the word") do
-      test_definitions = Definition.new(:description => 'it is a vehicle')
+      test_definitions = Definition.new({:description => 'it is a vehicle', :id =>1})
       expect(test_definitions.description()).to(eq("it is a vehicle"))
     end
   end
+
+  describe('.all') do
+    it("is empty at first") do
+      expect(Definition.all()).to(eq([]))
+    end
+  end
+
+  # describe('#save') do
+  #   it("saves a definiton to the array.") do
+  #     test_definitions = Definition.new({:description => 'it is a vehicle', :id =>1})
+  #     expect(Definition.all()).to(eq([test_definitions]))
+  #   end
+  # end
+
+
 
 
 
