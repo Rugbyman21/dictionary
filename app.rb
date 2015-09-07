aa077f15c8bb748eb0c2662d673563f7eb5f00ed
Rugbyman21 <trevorscherling@Trevors-MacBook-Air.local>
2015-09-06 17:36:37 -0700

    add get and post to app and added success files

--- app.rb
+++ app.rb
@@ -5,6 +5,26 @@ require('./lib/definition')
 also_reload('lib/**/*.rb')
 
 get('/') do
-  @@word = Word.all()
+  @word = Word.all()
   erb(:index)
+end
+
+post('/new_word') do
+  word_input = params.fetch('word_input')
+  new_word = Word.new(word_input)
+  new_word.save()
+  @word = Word.all
+  erb(:success)
+end
+
+get('/details/:id') do
+  @word = Word.find(params.fetch('id').to_i())
+  erb(:details)
+end
+
+post ("/details/:id") do
+  @new_definition = Definition.new(params.fetch('definition_input'))
+  @new_definition.save()
+  @word = Word.find(params.fetch('word_id').to_i()).add_definition(@new_definition)
+  erb(:success)
 end
